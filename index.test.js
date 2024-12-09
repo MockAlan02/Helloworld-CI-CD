290cdbbf72d4280ed0e2861ed48848df3a1a8462
MockAlan02 <alantubert09@gmail.com>
2024-12-08 21:46:00 -0400

    modified index test

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 test('HTML file should have <h1>Hola Mundo</h1>', () => {
     const htmlContent = fs.readFileSync('index.html', 'utf-8');
-    expect(htmlContent).toMatch(/<h1>Hola Mundo<\/h1>/);
+    expect(htmlContent).toMatch("<h1>Hello world</h1>");
 });
